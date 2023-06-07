ab5adda6f62b066581f7da2dd218c15c3b5c2dd5
parunchxi <parunchxi@gmail.com>
2023-06-07 09:47:56 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+// Change Theme
+const changeTheme = document.getElementById('change_theme');
+
+changeTheme.addEventListener('click', () => {
+    if (document.body.className === 'dark-mode') {
+        setLightMode();
+    } else {
+        setDarkMode();
+    }
+})
+
+function setDarkMode() {
+    document.body.classList.add('dark-mode');
+    changeTheme.innerHTML = '<i class="ri-moon-fill"></i>';
+    saveTeme('darkMode');
+}
+
+function setLightMode() {
+    document.body.classList.remove('dark-mode');
+    changeTheme.innerHTML = '<i class="ri-sun-fill"></i>';
+    saveTeme('lightMode');
+}
+
+// Get Theme
+const theme = localStorage.getItem('theme');
+
+if (theme === 'darkMode') {
+    setDarkMode();
+} else if (theme === 'lightMode') {
+    setLightMode();
+}
+
+// Save Theme
+function saveTeme(theme) {
+    localStorage.setItem('theme', theme);
+}
